0c944b82c1cdd2ed5910a843ad51e9628c5e659e
dmitryshundrik <dmitryshundrik@gmail.com>
2022-02-22 12:15:01 +0300

    Lesson5/Homework

---
+++ src/tms/lesson5/Main.java
@@ -0,0 +1,12 @@
+package tms.lesson5;
+
+public class Main {
+    public static void main(String[] args) {
+        new Task1().solution();
+        new Task1().solution();
+        new Task3().solution();
+        new Task4().solution();
+        new Task5().solution();
+        new Task6Game().startGame();
+    }
+}

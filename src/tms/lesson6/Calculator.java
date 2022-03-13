639df5a25be11baf91ab2fe3498c78793703d054
dmitryshundrik <dmitryshundrik@gmail.com>
2022-03-13 12:46:27 +0400

    Lesson6/Homework

---
+++ src/tms/lesson6/Calculator.java
@@ -0,0 +1,28 @@
+package tms.lesson6;
+
+public class Calculator {
+
+    double a;
+    double b;
+
+    public Calculator(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void sum() {
+        System.out.println(a + b);
+    }
+
+    public void subtraction() {
+        System.out.println(a - b);
+    }
+
+    public void multiple() {
+        System.out.println(a * b);
+    }
+
+    public void division() {
+        System.out.println(a / b);
+    }
+}

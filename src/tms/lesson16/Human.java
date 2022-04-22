433bacb46adc93a783d73c0ea88827ea70c9df36
dmitryshundrik <dmitryshundrik@gmail.com>
2022-04-22 18:21:33 +0400

    Lesson16/Homework

---
+++ src/tms/lesson16/Human.java
@@ -0,0 +1,42 @@
+package tms.lesson16;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Human {
+    @JsonProperty(value = "nameOfVisitor")
+    private String name;
+    @JsonProperty(value = "ageOfVisitor")
+    private String age;
+
+    public Human() {
+    }
+
+    public Human(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Human{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
